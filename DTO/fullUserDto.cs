61ea23a7f962fdbb3bc72dc26e456ab1e7050f7b
m97419 <m3197419@gmail.com>
2023-12-14 12:21:06 +0200

    validation

--- DTO/fullUserDto.cs
+++ DTO/fullUserDto.cs
@@ -13,10 +13,8 @@ namespace DTO
         [EmailAddress, StringLength(30)]
         public string? Email { get; set; }
 
-        [StringLength(15)]
         public string? FirstName { get; set; }
 
-        [StringLength(15)]
         public string? LastName { get; set; }
 
         [StringLength(15)]
